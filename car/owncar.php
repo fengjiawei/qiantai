0fd3d49f381b317aa1136e61d1a888709e03f841
jiangxiaogang <826251085@qq.com>
2016-04-06 14:12:22 +0800

    Signed-off-by: jiangxiaogang <826251085@qq.com>企业车主选择自有车辆

--- car/owncar.php
+++ car/owncar.php
@@ -2,13 +2,13 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2016/3/17
- * Time: 10:32
+ * Date: 2016/4/6
+ * Time: 10:26
+ * describe:自有车辆列表
  */
 require_once "function.php";
-//echo json_encode($_GET);
 
-$result = http_post_json(U."unite/enterprisePriceList.json",json_encode($_GET));
+$result = http_post_json(U."unite/queryOwnCar.json",json_encode($_GET));
 if (json_decode($result)->result == 'success') {
     echo json_encode(json_decode($result)->list);
 }
---
+++ good/lookCompany.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/4/6
+ * Time: 14:06
+ * describe:查看企业车主
+ */
+require_once "function.php";
+returnJson(U."unite/queryPublicTransport.json", $_GET);
