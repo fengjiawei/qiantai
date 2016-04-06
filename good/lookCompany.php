4c9c1dc4234a59bebea706e3681c149582402ca8
fengjiawei <1063554121@qq.com>
2016-04-06 10:28:07 +0800

    fengjiawei commit
    向企业推送货源消息,carlist.html v1.0

---
+++ good/lookCompany.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: fengjiawei
+ * Date: 16/4/6
+ * Time: 上午10:02
+ */
+require_once 'function.php';
+
+returnJson(U."unite/queryPublicTransport.json", $_GET);
