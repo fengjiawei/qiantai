dd72c81d211ee266c86a82cbdc1170599a3d8297
jiangxiaogang <826251085@qq.com>
2016-04-14 08:45:19 +0800

    Signed-off-by: jiangxiaogang <826251085@qq.com>车主查看回单

---
+++ car/lookImage.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/17
+ * Time: 9:51
+ * describe:查看回单
+ */
+require_once "function.php";
+returnJson(U."order/submitedGoods.json",$_POST);
